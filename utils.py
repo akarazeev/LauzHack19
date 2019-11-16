67d947af396fa0810175c470b565a8fed441afde
Samuel Prevost <samuel.prevost@etu.upmc.fr>
2019-11-16 21:03:47 +0100

    Now load_patients() returns two numpy arrays instead of a dict.

--- utils.py
+++ utils.py
@@ -1,15 +1,24 @@
 from Bio import SeqIO
 from Bio import Align
 import os
+import numpy as np
+from collections import OrderedDict
 
-
-def load_patients(root_data='data/dataset_cp_pt_brain_dna/DNA'):
-    patients = dict()
+def load_patients(root_data='data/dataset_cp_pt_brain_dna/DNA', return_dict=False):
+    patients = OrderedDict()
     for f in [f for f in os.listdir(root_data) if f.endswith('fastq')]:
         print(f, end="\t... ")
         patients[f.split('.')[0]] = list(SeqIO.parse(os.path.join(root_data, f), "fastq"))
         print("Done.")
-    return patients
+        
+    if return_dict:
+        return patients
+    else:
+        patients_entries = np.array([[str(entry.seq)] + [k] for k in patients.keys() for entry in patients[k]])
+        patients_identity = patients_entries[:, 1]
+        patients_entries = patients_entries[:, 0]
+
+        return patients_identity, patients_entries
     
     
 def load_viruses(root_data='data/viral'):
